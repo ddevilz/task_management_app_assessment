4cba3e7a11658df64316dfd63b889cb89fc977ef
Devashish <jadhavom24@gmail.com>
2024-07-23 15:51:36 +0530

    links updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api/v1"
+// export const BASE_URL = "http://localhost:3000/api/v1"
+export const BASE_URL =
+  "https://task-management-app-assessment.vercel.app/api/v1";
+
+//
